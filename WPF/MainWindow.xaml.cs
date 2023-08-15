6a49c0a5edb5d26f37ea40fec82047646df2d4de
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-08-15 23:48:53 +0500

    ss

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -57,6 +57,8 @@ namespace WpfApp1
                 MessageBox.Show("ww", "hello y all" , MessageBoxButton.YesNo , MessageBoxImage.Question);
                 MessageBox.Show("ww", "hellp you " , MessageBoxButton.YesNo , MessageBoxImage.Question);
                 MessageBox.Show("ww", "hellp you " , MessageBoxButton.YesNo , MessageBoxImage.Question);
+                MessageBox.Show("ww", "hellp you " , MessageBoxButton.YesNo , MessageBoxImage.Question);
+                MessageBox.Show("ww", "hellp you " , MessageBoxButton.YesNo , MessageBoxImage.Question);
                
                 // Store hashed password in database
                 //Record login and password in application settings property
